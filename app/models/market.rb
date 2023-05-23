fdf6598c096f73dcea8a724e50a4e313506c1c51
Anna Marie Sterling <sterling.am@gmail.com>
2023-05-23 02:07:21 -0600

    Model validations/relationships

---
+++ app/models/market.rb
@@ -0,0 +1,6 @@
+class Market < ApplicationRecord
+  validates :name, :street, :city, :county, :state, :zip, :lat, :lon, presence: true
+
+  has_many :market_vendors
+  has_many :vendors, through: :market_vendors
+end
