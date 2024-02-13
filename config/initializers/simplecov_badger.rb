2dedc81c91d1a3d92eb936390432656438cbed03
Anna Marie Sterling <sterling.am@gmail.com>
2024-02-13 15:04:43 -0700

    SimpleCov Badger

--- config/initializers/simplecov_badger.rb
+++ config/initializers/simplecov_badger.rb
@@ -1,6 +1,4 @@
-# SimpleCov::Badger.configure do |config|
-#   config.post_url = "coverage.traels.it/badges",
-#   config.repo_url = `git config --get remote.origin.url`.strip,
-#   config.run_if = -> { `git rev-parse --abbrev-ref HEAD` == "master\n" }
-#   config.token = ENV["SIMPLECOV_BADGER_TOKEN"]
-# end
+if Module.const_defined? "SimpleCov"
+  SimpleCov::Badger.configure do |config|
+  config.token = ENV["SIMPLECOV_BADGER_TOKEN"]  end
+end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -79,9 +79,9 @@ VCR.configure do |config|
   config.allow_http_connections_when_no_cassette = true
 end
 
-SimpleCov::Badger.configure do |config|
-  config.post_url = "coverage.traels.it/badges",
-  config.repo_url = `git config --get remote.origin.url`.strip,
-  config.run_if = -> { `git rev-parse --abbrev-ref HEAD` == "master\n" }
-  config.token = ENV["SIMPLECOV_BADGER_TOKEN"]
-end
+# SimpleCov::Badger.configure do |config|
+#   config.post_url = "coverage.traels.it/badges",
+#   config.repo_url = `git config --get remote.origin.url`.strip,
+#   config.run_if = -> { `git rev-parse --abbrev-ref HEAD` == "master\n" }
+#   config.token = ENV["SIMPLECOV_BADGER_TOKEN"]
+# end
